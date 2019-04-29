1017a618b73d4d6fefd9d5b3f917b616cde1e2d0
Boris Skert <boris.skert@adorsys.de>
2019-04-29 08:53:48 +0200

    Run ng within electron (#3)

---
+++ postinstall-electron.js
@@ -0,0 +1,16 @@
+// Allow angular using electron module (native node modules)
+const fs = require('fs');
+const f_angular = 'node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/webpack-configs/browser.js';
+
+fs.readFile(f_angular, 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  const result = data.replace(/target: "electron-renderer",/g, '')
+    .replace(/target: "web",/g, '')
+    .replace(/return {/g, 'return {target: "electron-renderer",');
+
+  fs.writeFile(f_angular, result, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+});
