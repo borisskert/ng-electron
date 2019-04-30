81b1a80f3ef448597090a4f0f66d75bed6781afa
Boris Skert <boris.skert@adorsys.de>
2019-04-30 16:47:36 +0200

    Fix unit tests

--- src/app/example/example-component/example.component.spec.ts
+++ src/app/example/example-component/example.component.spec.ts
@@ -1,6 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ExampleComponent } from './example.component';
+import { ExampleDataService } from '../service/example-data.service';
+import { Observable } from 'rxjs';
+import { ExampleData } from '../example-data.interface';
+import { Provider } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+class MockedExampleDataService implements ExampleDataService {
+  read(): Observable<ExampleData> {
+    return Observable.create('');
+  }
+
+  save(exampleData: ExampleData): Observable<void> {
+    return Observable.create(null);
+  }
+}
+
+const exampleDataProvider: Provider = {
+  provide: ExampleDataService,
+  useClass: MockedExampleDataService
+};
 
 describe('ExampleComponent', () => {
   let component: ExampleComponent;
@@ -8,7 +28,13 @@ describe('ExampleComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ExampleComponent ]
+      imports: [
+        FormsModule,
+      ],
+      declarations: [ ExampleComponent ],
+      providers: [
+        exampleDataProvider
+      ]
     })
       .compileComponents();
   }));
