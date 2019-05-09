6affc2cd3b7be6c63da3b9a2e7f82a3197140723
Boris Skert <boris.skert@adorsys.de>
2019-05-09 14:39:21 +0200

    [issue/#13] Auto-update application on startup

--- main.ts
+++ main.ts
@@ -2,15 +2,56 @@ import { app, BrowserWindow, screen } from 'electron';
 import * as path from 'path';
 import * as url from 'url';
 
+const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
+
+class AppUpdater {
+  constructor(private readonly sendStatusToWindow: (text: string) => void) {}
+
+  public init(): void {
+    log.transports.file.level = 'debug';
+    autoUpdater.logger = log;
+
+    autoUpdater.on('checking-for-update', () => {
+      this.sendStatusToWindow('Checking for update...');
+    });
+
+    autoUpdater.on('update-available', (info) => {
+      this.sendStatusToWindow('Update available.');
+    });
+
+    autoUpdater.on('update-not-available', (info) => {
+      this.sendStatusToWindow('Update not available.');
+    });
+
+    autoUpdater.on('error', (err) => {
+      this.sendStatusToWindow('Error in auto-updater. ' + err);
+    });
+
+    autoUpdater.on('download-progress', (progressObj) => {
+      let logMessage = 'Download speed: ' + progressObj.bytesPerSecond;
+      logMessage = logMessage + ' - Downloaded ' + progressObj.percent + '%';
+      logMessage = logMessage + ' (' + progressObj.transferred + '/' + progressObj.total + ')';
+      this.sendStatusToWindow(logMessage);
+    });
+
+    autoUpdater.on('update-downloaded', (info) => {
+      this.sendStatusToWindow('Update downloaded');
+    });
+
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+}
+
 (() => {
-  let window;
+  let browserWindow;
 
   function createWindow() {
     const args = process.argv.slice(1);
     const serve = args.some(val => val === '--serve');
     const size = screen.getPrimaryDisplay().workAreaSize;
 
-    window = new BrowserWindow({
+    browserWindow = new BrowserWindow({
       x: 0,
       y: 0,
       width: size.width,
@@ -24,9 +65,9 @@ import * as url from 'url';
       require('electron-reload')(__dirname, {
         electron: require(`${__dirname}/node_modules/electron`)
       });
-      window.loadURL('http://localhost:4200');
+      browserWindow.loadURL('http://localhost:4200');
     } else {
-      window.loadURL(url.format({
+      browserWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'dist/index.html'),
         protocol: 'file:',
         slashes: true
@@ -34,23 +75,36 @@ import * as url from 'url';
     }
 
     if (serve) {
-      window.webContents.openDevTools();
+      browserWindow.webContents.openDevTools();
     }
 
     // Emitted when the window is closed.
-    window.on('closed', () => {
+    browserWindow.on('closed', () => {
       // Dereference the window object, usually you would store window
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
-      window = null;
+      browserWindow = null;
     });
   }
+
+  function sendStatusToWindow(text) {
+    log.info(text);
+
+    if (browserWindow && browserWindow.webContents) {
+      browserWindow.webContents.send('message', text);
+    }
+  }
+
+  const appUpdater = new AppUpdater(sendStatusToWindow);
 
   try {
     // This method will be called when Electron has finished
     // initialization and is ready to create browser windows.
     // Some APIs can only be used after this event occurs.
-    app.on('ready', createWindow);
+    app.on('ready', () => {
+      createWindow();
+      appUpdater.init();
+    });
 
     // Quit when all windows are closed.
     app.on('window-all-closed', () => {
@@ -64,12 +118,12 @@ import * as url from 'url';
     app.on('activate', () => {
       // On OS X it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
-      if (window === null) {
+      if (browserWindow === null) {
         createWindow();
       }
     });
   } catch (e) {
-    console.log('Should never ever happen:', e);
+    log.info('Fatal error occured while starting app:', e);
     throw e;
   }
 })();
